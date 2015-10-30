a90db0897a8fd693f46a104f05c7ae226d356085
ThomasCrvsr <crevoisier.thomas@gmail.com>
2015-10-30 15:40:58 +0100

    Déplacement du Header dans un composant

---
+++ app/Header.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+function Header () {
+  return (
+    <header className="col-md-12">
+        <h2>ReactMovie</h2>
+    </header>
+  );
+}
+
+module.exports = Header;
