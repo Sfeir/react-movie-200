48903e5e3a12d97f2e4f9c50e68aae15063dbe2c
ThomasCrvsr <crevoisier.thomas@gmail.com>
2015-12-11 15:48:56 +0100

    Affichage des boutons d'action à la sélection

--- app/Movie.jsx
+++ app/Movie.jsx
@@ -1,23 +1,46 @@
 var React = require('react');
 
-function Movie (props) {
-  var film = props.film,
-    afficheUrl = film.afficheUrl || 'img/no-poster.jpg';
+var Movie = React.createClass({
+  getInitialState: function () {
+    return {
+      selected: false
+    }
+  },
 
-  return (
-    <li className="col-md-12">
-      <img src={afficheUrl} className="col-md-2" />
-      <div className="caption pull-left">
-        <h3>{film.titre}</h3>
-        <p><b>Acteurs : </b>{film.acteurs}</p>
-        <p><b>Synopsis : </b>{film.synopsis}</p>
-      </div>
-      <div className="pull-right">
-        <button className="btn btn-default" onClick={props.onMovieDeletion.bind(null, film.id)}>&times;</button>
-      </div>
-    </li>
-  );
-}
+  onSelect: function () {
+    this.setState({
+      selected: true
+    });
+  },
+
+  render: function () {
+    var film = this.props.film,
+      afficheUrl = film.afficheUrl || 'img/no-poster.jpg',
+      actionButtons;
+
+    if (this.state.selected) {
+      actionButtons = (
+        <div className="pull-right">
+          <button className="btn btn-default" onClick={this.props.onMovieDeletion.bind(null, film.id)}>&times;</button>
+        </div>
+      );
+    } else {
+      actionButtons = false;
+    }
+
+    return (
+      <li className="col-md-12" onClick={this.onSelect}>
+        <img src={afficheUrl} className="col-md-2" />
+        <div className="caption pull-left">
+          <h3>{film.titre}</h3>
+          <p><b>Acteurs : </b>{film.acteurs}</p>
+          <p><b>Synopsis : </b>{film.synopsis}</p>
+        </div>
+        {actionButtons}
+      </li>
+    );
+  }
+});
 
 
 module.exports = Movie;
