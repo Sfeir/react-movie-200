d5f09323016362dcd9ae80c01fc102571ac443b3
ThomasCrvsr <crevoisier.thomas@gmail.com>
2015-12-11 15:23:12 +0100

    Déplacement de la liste des films dans le state de App

--- app/App.jsx
+++ app/App.jsx
@@ -4,13 +4,63 @@ var Header = require('./Header.jsx');
 var MovieList = require('./MovieList.jsx');
 var SearchBar = require('./SearchBar.jsx');
 
+var MOVIES = [
+  {
+    id: 1,
+    titre: 'AVATAR',
+    afficheUrl: 'img/avatar.jpg',
+    acteurs: 'Bruce Willis, Bruce Willis & Bruce Willis',
+    synopsis: 'Il se passe des trucs dans ce film'
+  },
+  {
+    id: 2,
+    titre: 'REC',
+    afficheUrl: 'img/rec.jpg',
+    acteurs: "Des gens, et d'autres gens.",
+    synopsis: 'Il se passe aussi des trucs dans ce film'
+  }
+];
+
 var App = React.createClass({
+  getInitialState: function () {
+    return {
+      movies: [],
+      loadingMovies: false
+    }
+  },
+
+  componentWillMount: function () {
+    this.setState({
+      loadingMovies: true
+    });
+
+    setTimeout(function (context) {
+      context.setState({
+        movies: MOVIES,
+        loadingMovies: false
+      });
+    }, 1000, this);
+  },
+
+  onMovieDeletion: function (movieId) {
+    var filteredMovieList = this.state.movies.filter(function (movie) {
+      return movie.id !== movieId;
+    });
+
+    this.setState({
+      movies: filteredMovieList
+    });
+  },
+
   render: function () {
     return (
       <div>
         <Header />
         <SearchBar />
-        <MovieList />
+        <MovieList
+            movies={this.state.movies}
+            loadingMovies={this.state.loadingMovies}
+            onMovieDeletion={this.onMovieDeletion} />
       </div>
     );
   }
--- app/MovieList.jsx
+++ app/MovieList.jsx
@@ -1,63 +1,18 @@
 var React = require('react');
 var Movie = require('./Movie.jsx');
 
-var MOVIES = [
-  {
-    id: 1,
-    titre: 'AVATAR',
-    afficheUrl: 'img/avatar.jpg',
-    acteurs: 'Bruce Willis, Bruce Willis & Bruce Willis',
-    synopsis: 'Il se passe des trucs dans ce film'
-  },
-  {
-    id: 2,
-    titre: 'REC',
-    afficheUrl: 'img/rec.jpg',
-    acteurs: "Des gens, et d'autres gens.",
-    synopsis: 'Il se passe aussi des trucs dans ce film'
-  }
-]
+
 
 var MovieList = React.createClass({
-  getInitialState: function () {
-    return {
-      movies: [],
-      loading: false
-    }
-  },
-
-  componentWillMount: function () {
-    this.setState({
-      loading: true
-    });
-
-    setTimeout(function (context) {
-      context.setState({
-        movies: MOVIES,
-        loading: false
-      });
-    }, 1000, this);
-  },
-
-  onMovieDeletion: function (movieId) {
-    var filteredMovieList = this.state.movies.filter(function (movie) {
-      return movie.id !== movieId;
-    });
-
-    this.setState({
-      movies: filteredMovieList
-    });
-  },
-
   render: function () {
-    var movies = this.state.movies;
-    var onMovieDeletion = this.onMovieDeletion;
+    var movies = this.props.movies;
+    var onMovieDeletion = this.props.onMovieDeletion;
     var moviesTag = movies.map(function (movie) {
       return <Movie film={movie} onMovieDeletion={onMovieDeletion} />
     });
     var content;
 
-    if (this.state.loading) {
+    if (this.props.loadingMovies) {
       content = <li>Chargement de la liste des films en cours</li>
     } else {
       content = moviesTag;
