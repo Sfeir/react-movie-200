c79945a46feeb3dac5cf24b1e8123c931e85740f
ThomasCrvsr <crevoisier.thomas@gmail.com>
2015-12-18 16:44:43 +0100

    Fix composant movie

--- app/Movie.jsx
+++ app/Movie.jsx
@@ -2,15 +2,18 @@ var React = require('react');
 
 function Movie (props) {
   var film = props.film,
-    afficheUrl = film.afficheUrl || 'img/no-poster.jpg';
+    afficheUrl = film.poster || 'img/no-poster.jpg';
 
   return (
     <li className="col-md-12">
       <img src={afficheUrl} className="col-md-2" />
       <div className="caption">
-        <h3>{film.titre}</h3>
-        <p><b>Acteurs : </b>{film.acteurs}</p>
+        <h3>{film.title}</h3>
+        <p><b>Année de sortie : </b>{film.releaseYear}</p>
+        <p><b>Réalisateurs : </b>{film.directors}</p>
+        <p><b>Acteurs : </b>{film.actors}</p>
         <p><b>Synopsis : </b>{film.synopsis}</p>
+        <p><b>Prix : </b>{film.price} €</p>
       </div>
     </li>
   );
