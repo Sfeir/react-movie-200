441e28f2c7d8d55cb142c3dd4c421241ce7ed65d
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-01-01 13:35:00 +0100

    Update variable MOVIES

--- app/MovieList.jsx
+++ app/MovieList.jsx
@@ -3,20 +3,36 @@ var Movie = require('./Movie.jsx');
 
 var MOVIES = [
   {
-    id: 1,
-    titre: 'AVATAR',
-    afficheUrl: 'img/avatar.jpg',
-    acteurs: 'Bruce Willis, Bruce Willis & Bruce Willis',
-    synopsis: 'Il se passe des trucs dans ce film'
-  },
-  {
-    id: 2,
-    titre: 'REC',
-    afficheUrl: 'img/rec.jpg',
-    acteurs: "Des gens, et d'autres gens.",
-    synopsis: 'Il se passe aussi des trucs dans ce film'
-  }
-]
+		id: 1,
+		title : "Avatar",
+		category : 'sciencesfiction',
+		releaseYear : "2010",
+		poster : "img/avatar.jpg",
+		directors : "James Cameron",
+		actors : "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang, Michelle Rodriguez",
+		synopsis : "Sur la lointaine planète de Pandora, Jake Sully, un héros malgré lui, " +
+		"se lance dans une quête de rédemption, de découverte, d'amour inattendu, dont l'issue sera un " +
+		"combat héroïque pour sauver toute une civilisation.",
+		rate : 3,
+		lastViewDate : new Date(2013, 3, 1, 12, 4, 50),
+		price : 25.46
+	},
+	{
+		id: 2,
+		title : "Seigneur des Anneaux : La Communauté de l'Anneau",
+		category : 'sciencesfiction',
+		releaseYear : "2001",
+		poster : "img/seigneurdesanneaux1.jpg",
+		directors : "Peter Jackson",
+		actors : "Elijah Wood, Sean Astin, Ian McKellen, Sala Baker, Viggo Mortensen",
+		synopsis : "Frodon le Hobbit hérite de l'Anneau Unique, un instrument de pouvoir absolu" +
+		"qui permettrait à Sauron, le Seigneur des ténèbres, de régner sur la Terre du Milieu." +
+		" Commence alors un vaste périple visant à la destruction de l'objet.",
+		rate : 5,
+		lastViewDate : new Date(2012, 5, 13, 9, 30, 45),
+		price : 34.76
+	}
+];
 
 function MovieList () {
   var movies = MOVIES.map(function (movie) {
