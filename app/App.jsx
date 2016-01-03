4ab5536b1c0df4e43b593dfe9bb78629e21aced6
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-01-03 21:49:47 +0100

    Fix #4 : Suppression var MOVIES inutilisée

--- app/App.jsx
+++ app/App.jsx
@@ -5,23 +5,6 @@ var MovieList = require('./MovieList.jsx');
 var SearchBar = require('./SearchBar.jsx');
 var MovieAPI = require('./api/MovieAPI');
 
-var MOVIES = [
-  {
-    id: 1,
-    titre: 'AVATAR',
-    afficheUrl: 'img/avatar.jpg',
-    acteurs: 'Bruce Willis, Bruce Willis & Bruce Willis',
-    synopsis: 'Il se passe des trucs dans ce film'
-  },
-  {
-    id: 2,
-    titre: 'REC',
-    afficheUrl: 'img/rec.jpg',
-    acteurs: "Des gens, et d'autres gens.",
-    synopsis: 'Il se passe aussi des trucs dans ce film'
-  }
-];
-
 var App = React.createClass({
   getInitialState: function () {
     return {
