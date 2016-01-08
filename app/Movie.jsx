1db71f6741f9be6026ca37ecae39859eaf5d2b58
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-01-08 14:42:13 +0100

    Utilisation de React.createClass au lieu de dumb components

--- app/Movie.jsx
+++ app/Movie.jsx
@@ -1,26 +1,27 @@
 var React = require('react');
 
-function Movie (props) {
-  var film = props.film,
-    afficheUrl = film.poster || 'img/no-poster.jpg';
+var Movie = React.createClass({
+  render: function () {
+    var film = this.props.film,
+      afficheUrl = film.poster || 'img/no-poster.jpg';
 
-  return (
-    <li className="col-md-12">
-      <img src={afficheUrl} className="col-md-2" />
-      <div className="caption col-md-8 pull-left">
-        <h3>{film.title}</h3>
-        <p><b>Année de sortie : </b>{film.releaseYear}</p>
-        <p><b>Réalisateurs : </b>{film.directors}</p>
-        <p><b>Acteurs : </b>{film.actors}</p>
-        <p><b>Synopsis : </b>{film.synopsis}</p>
-        <p><b>Prix : </b>{film.price} €</p>
-      </div>
-      <div className="pull-right">
-        <button className="btn btn-danger" onClick={props.onMovieDeletion.bind(null, film.id)}><i className="glyphicon glyphicon-trash"></i></button>
-      </div>
-    </li>
-  );
-}
-
+    return (
+      <li className="col-md-12">
+        <img src={afficheUrl} className="col-md-2" />
+        <div className="caption col-md-8 pull-left">
+          <h3>{film.title}</h3>
+          <p><b>Année de sortie : </b>{film.releaseYear}</p>
+          <p><b>Réalisateurs : </b>{film.directors}</p>
+          <p><b>Acteurs : </b>{film.actors}</p>
+          <p><b>Synopsis : </b>{film.synopsis}</p>
+          <p><b>Prix : </b>{film.price} €</p>
+        </div>
+        <div className="pull-right">
+          <button className="btn btn-danger" onClick={this.props.onMovieDeletion.bind(null, film.id)}><i className="glyphicon glyphicon-trash"></i></button>
+        </div>
+      </li>
+    );
+  }
+});
 
 module.exports = Movie;
