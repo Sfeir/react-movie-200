8ab96567250a094705898ff5d22df1754b50216a
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-01-08 11:51:49 +0100

    Fix #10 : Utilisation d'axios pour la suppression de film

--- app/MovieList.jsx
+++ app/MovieList.jsx
@@ -24,17 +24,15 @@ var MovieList = React.createClass({
   },
 
   onMovieDeletion: function (movieId) {
-    var ctx = this;
-
-    MovieAPI.removeMovie(movieId, function () {
-      var filteredMovieList = ctx.state.movies.filter(function (movie) {
+    MovieAPI.removeMovie(movieId).then(function () {
+      var filteredMovieList = this.state.movies.filter(function (movie) {
         return movie.id !== movieId;
       });
 
-      ctx.setState({
+      this.setState({
         movies: filteredMovieList
       });
-    });
+    }.bind(this));
   },
 
   render: function () {
