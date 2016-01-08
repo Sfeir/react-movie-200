3aca8385512afb6c0d81d396e1903e460abfd0f3
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-01-08 14:37:41 +0100

    Utilisation de React.createClass au lieu de dumb components

--- app/Movie.jsx
+++ app/Movie.jsx
@@ -1,23 +1,24 @@
 var React = require('react');
 
-function Movie (props) {
-  var film = props.film,
-    afficheUrl = film.poster || 'img/no-poster.jpg';
+var Movie = React.createClass({
+  render: function () {
+    var film = this.props.film,
+      afficheUrl = film.poster || 'img/no-poster.jpg';
 
-  return (
-    <li className="col-md-12">
-      <img src={afficheUrl} className="col-md-2" />
-      <div className="caption">
-        <h3>{film.title}</h3>
-        <p><b>Année de sortie : </b>{film.releaseYear}</p>
-        <p><b>Réalisateurs : </b>{film.directors}</p>
-        <p><b>Acteurs : </b>{film.actors}</p>
-        <p><b>Synopsis : </b>{film.synopsis}</p>
-        <p><b>Prix : </b>{film.price} €</p>
-      </div>
-    </li>
-  );
-}
-
+    return (
+      <li className="col-md-12">
+        <img src={afficheUrl} className="col-md-2" />
+        <div className="caption">
+          <h3>{film.title}</h3>
+          <p><b>Année de sortie : </b>{film.releaseYear}</p>
+          <p><b>Réalisateurs : </b>{film.directors}</p>
+          <p><b>Acteurs : </b>{film.actors}</p>
+          <p><b>Synopsis : </b>{film.synopsis}</p>
+          <p><b>Prix : </b>{film.price} €</p>
+        </div>
+      </li>
+    );
+  }
+});
 
 module.exports = Movie;
