c4df1ed0bbf3c93d1d7583dbd639904851495e94
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-01-15 11:17:59 +0100

    Fix refs formulaire

--- app/components/MovieForm.jsx
+++ app/components/MovieForm.jsx
@@ -12,9 +12,13 @@ var MovieForm = React.createClass({
   },
 
   componentDidMount: function () {
-    this.refs.movieTitle.value = this.props.movie.title;
-    this.refs.movieActors.value = this.props.movie.actors;
-    this.refs.movieSynopsis.value = this.props.movie.synopsis;
+    var movie = this.props.movie || {};
+    this.refs.movieTitle.value = movie.title || '';
+    this.refs.movieReleaseYear.value = movie.releaseYear || '';
+    this.refs.movieDirectors.value = movie.directors || '';
+    this.refs.movieActors.value = movie.actors || '';
+    this.refs.movieSynopsis.value = movie.synopsis || '';
+    this.refs.movieRate.value = movie.rate || 0;
   },
 
   onSubmit: function (e) {
@@ -22,8 +26,11 @@ var MovieForm = React.createClass({
 
     this.props.onMovieFormSaved({
       title: this.refs.movieTitle.value,
+      releaseYear: this.refs.movieReleaseYear.value,
+      directors: this.refs.movieDirectors.value,
       actors: this.refs.movieActors.value,
       synopsis: this.refs.movieSynopsis.value,
+      rate: this.refs.movieRate.value
     });
 
     this.resetForm();
@@ -31,8 +38,11 @@ var MovieForm = React.createClass({
 
   resetForm: function () {
     this.refs.movieTitle.value = '';
+    this.refs.movieReleaseYear.value = '';
+    this.refs.movieDirectors.value = '';
     this.refs.movieActors.value = '';
     this.refs.movieSynopsis.value = '';
+    this.refs.movieRate.value = 0;
   },
 
   render: function () {
@@ -44,37 +54,37 @@ var MovieForm = React.createClass({
 					<div className="form-group">
 						<label className="col-sm-4 control-label">Titre :</label>
 						<div className="col-sm-7">
-							<input type="text" ref="titleInput" className="form-control" required/>
+							<input type="text" ref="movieTitle" className="form-control" required/>
 						</div>
 					</div>
 					<div className="form-group">
 						<label className="col-sm-4 control-label">Année de sortie :</label>
 						<div className="col-sm-7">
-							<input type="text" className="form-control"/>
+							<input type="text" ref="movieReleaseYear" className="form-control"/>
 						</div>
 					</div>
 					<div className="form-group">
 						<label className="col-sm-4 control-label">Réalisateur : </label>
 						<div className="col-sm-7">
-							<input type="text" className="form-control"/>
+							<input type="text" ref="movieDirectors" className="form-control"/>
 						</div>
 					</div>
 					<div className="form-group">
 						<label className="col-sm-4 control-label">Acteurs :</label>
 						<div className="col-sm-7">
-							<input type="text" className="form-control"/>
+							<input type="text" ref="movieActors" className="form-control"/>
 						</div>
 					</div>
 					<div className="form-group">
 						<label className="col-sm-4 control-label">Synopsis :</label>
 						<div className="col-sm-7">
-							<textarea type="text" className="form-control" ></textarea>
+							<textarea type="text" ref="movieSynopsis" className="form-control" ></textarea>
 						</div>
 					</div>
 					<div className="form-group">
 						<label className="col-sm-4 control-label">Note :</label>
 						<div className="col-sm-7">
-							<input type="number" className="form-control" ref="rateInput" placeholder="entre 1 et 5" min="0" max="5"/>
+							<input type="number" ref="movieRate" className="form-control" placeholder="entre 1 et 5" min="0" max="5"/>
 						</div>
 					</div>
           <div className="form-group">
