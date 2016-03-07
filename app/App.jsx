414f6ae5a4cd162d2f784b3914d9d19511768d29
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-03-07 23:27:41 +0100

    Déplacement de la liste des films dans le state de App

--- app/App.jsx
+++ app/App.jsx
@@ -3,14 +3,50 @@ var ReactDOM = require('react-dom');
 var Header = require('./Header.jsx');
 var MovieList = require('./MovieList.jsx');
 var SearchBar = require('./SearchBar.jsx');
+var MovieAPI = require('./api/MovieAPI.js');
 
 var App = React.createClass({
+  getInitialState: function () {
+    return {
+      movies: [],
+      loadingMovies: false
+    }
+  },
+
+  componentWillMount: function () {
+    this.setState({
+      loadingMovies: true
+    });
+
+    MovieAPI.getMovieList().then(function (movies) {
+      this.setState({
+        movies: movies,
+        loadingMovies: false
+      });
+    }.bind(this));
+  },
+
+  onMovieDeletion: function (movieId) {
+    MovieAPI.removeMovie(movieId).then(function () {
+      var filteredMovieList = this.state.movies.filter(function (movie) {
+        return movie.id !== movieId;
+      });
+
+      this.setState({
+        movies: filteredMovieList
+      })
+    }.bind(this));
+  },
+
   render: function () {
     return (
       <div>
         <Header />
         <SearchBar />
-        <MovieList />
+        <MovieList
+          movies={this.state.movies}
+          loadingMovies={this.state.loadingMovies}
+          onMovieDeletion={this.onMovieDeletion} />
       </div>
     );
   }
--- app/MovieList.jsx
+++ app/MovieList.jsx
@@ -1,48 +1,15 @@
 var React = require('react');
 var Movie = require('./Movie.jsx');
-var MovieAPI = require('./api/MovieAPI.js');
 
 var MovieList = React.createClass({
-  getInitialState: function () {
-    return {
-      movies: [],
-      loading: false
-    }
-  },
-
-  componentWillMount: function () {
-    this.setState({
-      loading: true
-    });
-
-    MovieAPI.getMovieList().then(function (movies) {
-      this.setState({
-        movies: movies,
-        loading: false
-      });
-    }.bind(this));
-  },
-
-  onMovieDeletion: function (movieId) {
-    MovieAPI.removeMovie(movieId).then(function () {
-      var filteredMovieList = this.state.movies.filter(function (movie) {
-        return movie.id !== movieId;
-      });
-
-      this.setState({
-        movies: filteredMovieList
-      })
-    }.bind(this));
-  },
-
   render: function () {
-    var movies = this.state.movies;
+    var movies = this.props.movies;
     var moviesTag = movies.map(function (movie) {
-      return <Movie data={movie} onMovieDeletion={this.onMovieDeletion} />
+      return <Movie data={movie} onMovieDeletion={this.props.onMovieDeletion} />
     }.bind(this));
     var content;
 
-    if (this.state.loading) {
+    if (this.props.loadingMovies) {
       content = <li>Chargement des films en cours</li>
     } else {
       content = moviesTag;
