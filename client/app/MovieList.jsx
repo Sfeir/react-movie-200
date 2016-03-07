2698bebc4c57dee8d0ff8fcf8fd521c1b980d014
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-03-07 22:28:14 +0100

    Déplacement de la liste de films dans un composant

---
+++ client/app/MovieList.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+var MovieList = React.createClass({
+  render: function () {
+    return (
+      <ul className="thumbnails list-unstyled">
+          <li className="col-md-12 row">
+              <img src="server/img/avatar.jpg" className="col-md-2" />
+              <div className="caption">
+                  <h3>AVATAR</h3>
+                  <p><b>Acteurs : </b>Bruce Willis, Bruce Willis & Bruce Willis</p>
+                  <p><b>Synopsis : </b>Il se passe des trucs dans ce film</p>
+              </div>
+          </li>
+
+          <li className="col-md-12 row">
+              <img src="server/img/rec.jpg" className="col-md-2" />
+              <div className="caption">
+                  <h3>REC</h3>
+                  <p><b>Acteurs : </b>Des gens, et d'autres gens.</p>
+                  <p><b>Synopsis : </b>Il se passe aussi des trucs dans ce film</p>
+              </div>
+          </li>
+      </ul>
+    );
+  }
+});
+
+module.exports = MovieList;
