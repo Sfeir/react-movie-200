ca1727c23d0df71489798cda4c668e9e611213db
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-03-07 22:18:20 +0100

    Déplacement du contenu de l'application dans un composant root

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -5,3 +5,43 @@ require('./App.css');
 /** Polyfills */
 require('babel-polyfill');
 require('whatwg-fetch');
+
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <header className="col-md-12">
+                    <h2>ReactMovie</h2>
+                </header>
+
+                <ul className="thumbnails list-unstyled">
+                    <li className="col-md-12 row">
+                        <img src="server/img/avatar.jpg" className="col-md-2" />
+                        <div className="caption">
+                            <h3>AVATAR</h3>
+                            <p><b>Acteurs : </b>Bruce Willis, Bruce Willis & Bruce Willis</p>
+                            <p><b>Synopsis : </b>Il se passe des trucs dans ce film</p>
+                        </div>
+                    </li>
+
+                    <li className="col-md-12 row">
+                        <img src="server/img/rec.jpg" className="col-md-2" />
+                        <div className="caption">
+                            <h3>REC</h3>
+                            <p><b>Acteurs : </b>Des gens, et d'autres gens.</p>
+                            <p><b>Synopsis : </b>Il se passe aussi des trucs dans ce film</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+});
+
+
+ReactDOM.render(<App />, document.getElementById('main'));
