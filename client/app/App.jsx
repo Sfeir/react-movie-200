f6516286e278243c9e09d02094580cf04bd5a021
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-03-07 23:32:01 +0100

    Implémentation recherche

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -10,7 +10,6 @@ require('whatwg-fetch');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-
 var Header = require('./Header.jsx');
 var MovieList = require('./MovieList.jsx');
 var SearchBar = require('./SearchBar.jsx');
@@ -18,50 +17,62 @@ var MovieAPI = require('./api/MovieAPI.js');
 
 
 var App = React.createClass({
-    getInitialState: function () {
-        return {
-            movies: [],
-            loadingMovies: false
-        }
-    },
-
-    componentWillMount: function () {
-        this.setState({
-            loadingMovies: true
-        });
-
-        MovieAPI.getMovieList().then(function (movies) {
-            this.setState({
-                movies: movies,
-                loadingMovies: false
-            });
-        }.bind(this));
-    },
-
-    onMovieDeletion: function (movieId) {
-        MovieAPI.removeMovie(movieId).then(function () {
-            var filteredMovieList = this.state.movies.filter(function (movie) {
-                return movie.id !== movieId;
-            });
-
-            this.setState({
-                movies: filteredMovieList
-            })
-        }.bind(this));
-    },
-
-    render: function () {
-        return (
-            <div>
-                <Header />
-                <SearchBar />
-                <MovieList
-                    movies={this.state.movies}
-                    loadingMovies={this.state.loadingMovies}
-                    onMovieDeletion={this.onMovieDeletion} />
-            </div>
-        );
+  getInitialState: function () {
+    return {
+      movies: [],
+      loadingMovies: false,
+      searchKey: ''
     }
+  },
+
+  componentWillMount: function () {
+    this.setState({
+      loadingMovies: true
+    });
+
+    MovieAPI.getMovieList().then(function (movies) {
+      this.setState({
+        movies: movies,
+        loadingMovies: false
+      });
+    }.bind(this));
+  },
+
+  onMovieDeletion: function (movieId) {
+    MovieAPI.removeMovie(movieId).then(function () {
+      var filteredMovieList = this.state.movies.filter(function (movie) {
+        return movie.id !== movieId;
+      });
+
+      this.setState({
+        movies: filteredMovieList
+      })
+    }.bind(this));
+  },
+
+  onSearch: function (searchKey) {
+    this.setState({
+      searchKey: searchKey
+    });
+  },
+
+  render: function () {
+    var searchKey = this.state.searchKey;
+    var displayedMovies = this.state.movies.filter(function (movie) {
+      return movie.title.toLowerCase().match(searchKey.toLowerCase());
+    });
+
+    return (
+      <div>
+        <Header />
+        <SearchBar onSearch={this.onSearch} />
+        <MovieList
+          movies={displayedMovies}
+          loadingMovies={this.state.loadingMovies}
+          onMovieDeletion={this.onMovieDeletion} />
+      </div>
+    );
+  }
 });
 
 
