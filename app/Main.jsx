9ea9762d2e78317d702b4c53517709e13abb14f4
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-03-08 22:57:02 +0100

    Bootstrap routing

--- app/Main.jsx
+++ app/Main.jsx
@@ -1,8 +1,27 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./App.jsx');
+var Home = require('./Home.jsx');
+
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+
+var createHashHistory = require('history/lib/createHashHistory');
+var history = createHashHistory({
+  queryKey: false
+});
 
 // Faire le routing ici
+var Main = React.createClass({
+  render: function () {
+    return (
+      <Router history={history}>
+        <Route path="/" component={App} />
+      </Router>
+    );
+  }
+});
 
-
-ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(<Main />, document.getElementById('main'));
