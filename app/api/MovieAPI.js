bd241a94b289ce76283fa10fcef54666295008e5
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-03-08 23:22:17 +0100

    Fix composant Movie

--- app/api/MovieAPI.js
+++ app/api/MovieAPI.js
@@ -18,9 +18,15 @@ function updateMovie (movie) {
   return axios.put('/server/api/movies/' + movie.id, movie);
 }
 
+function getMovie (id) {
+  return axios.get('/server/api/movies/' + id)
+          .then(function (response) { return response.data; });
+}
+
 module.exports = {
   getMovieList: getMovieList,
   removeMovie: removeMovie,
   addMovie: addMovie,
-  updateMovie: updateMovie
+  updateMovie: updateMovie,
+  getMovie: getMovie
 }
