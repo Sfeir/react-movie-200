427659e552a087ec48e7f8ceac66e5b089d30223
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-05-30 22:34:03 +0200

    Utilisation de flux pour la liste des films

--- client/app/actions/MoviesActionCreator.js
+++ client/app/actions/MoviesActionCreator.js
@@ -1,6 +1,15 @@
 var dispatcher = require('../dispatcher');
 var actionTypes = require('./actionTypes');
+var MovieAPI = require('../api/MovieAPI');
 
 module.exports = {
-
+	fetchMovies: function () {
+		MovieAPI.getMovieList()
+			.then(function (movies) {
+				dispatcher.dispatch({
+					actionType: actionTypes.FETCH_MOVIES,
+					movies: movies
+				});
+			});
+	}
 };
