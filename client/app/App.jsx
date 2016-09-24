89e11a7116e0ceab423d32e8143762ab757b31f2
Thomas Winckell <thomas.winckell@gmail.com>
2016-09-24 19:02:57 +0200

    Déplacement de la liste des films dans le state de App

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -9,18 +9,49 @@ import 'whatwg-fetch';
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
-import Header       from './Header';
-import SearchBar    from './SearchBar';
-import MovieList    from './MovieList';
+import Header           from './Header';
+import SearchBar        from './SearchBar';
+import MovieList        from './MovieList';
+import * as MovieApi    from './api/MovieApi';
 
 export default class App extends React.Component {
+
+    state = {
+        movies  : [],
+        loadingMovies : false
+    };
+
+    componentWillMount() {
+        this.setState({
+            loadingMovies : true
+        });
+        MovieApi.getMovieList().then(movies => {
+            this.setState({
+                movies,
+                loadingMovies : false
+            });
+        });
+    }
+
+    onMovieDeletion(movieId) {
+        MovieApi.removeMovie(movieId).then(() => {
+            const filteredMovieList = this.state.movies.filter(movie => movie.id !== movieId);
+            this.setState({
+                movies : filteredMovieList
+            });
+        });
+    }
 
     render() {
         return (
             <div>
                 <Header/>
                 <SearchBar/>
-                <MovieList/>
+                <MovieList
+                    movies={this.state.movies}
+                    loadingMovies={this.state.loadingMovies}
+                    onMovieDeletion={this.onMovieDeletion.bind(this)}
+                />
             </div>
         );
     }
--- client/app/MovieList.jsx
+++ client/app/MovieList.jsx
@@ -1,45 +1,18 @@
 import React from 'react';
 
 import Movie from './Movie';
-import * as MovieApi from './api/MovieApi';
 
 
 export default class MovieList extends React.Component {
 
-    state = {
-        movies  : [],
-        loading : false
-    };
-
-    componentWillMount() {
-        this.setState({
-            loading : true
-        });
-        MovieApi.getMovieList().then(movies => {
-            this.setState({
-                movies,
-                loading : false
-            });
-        });
-    }
-
-    onMovieDeletion(movieId) {
-        MovieApi.removeMovie(movieId).then(() => {
-            const filteredMovieList = this.state.movies.filter(movie => movie.id !== movieId);
-            this.setState({
-                movies : filteredMovieList
-            });
-        });
-    }
-
     renderMovie(movie) {
         return (
-            <Movie data={movie} onMovieDeletion={this.onMovieDeletion.bind(this)} />
+            <Movie data={movie} onMovieDeletion={this.props.onMovieDeletion} />
         );
     }
 
     render() {
-        const content = this.state.loading ? <li>Chargement des films</li> : this.state.movies.map(this.renderMovie.bind(this));
+        const content = this.props.loadingMovies ? <li>Chargement des films</li> : this.props.movies.map(this.renderMovie.bind(this));
         return (
             <ul className="thumbnails list-unstyled">
                 {content}
