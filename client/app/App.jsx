a12c5b36c2d574897e9a22f2a2fd23ae60cf6b9b
Thomas Winckell <thomas.winckell@gmail.com>
2016-09-24 13:59:58 +0200

    Déplacement du contenu de l'application dans un composant root

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -5,3 +5,41 @@ import './App.css';
 /** Polyfills */
 import 'babel-polyfill';
 import 'whatwg-fetch';
+
+import React    from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <header className="col-md-12">
+                    <h2>ReactMovie</h2>
+                </header>
+
+                <ul className="thumbnails list-unstyled">
+                    <li className="col-md-12 row">
+                        <img src="server/img/avatar.jpg" className="col-md-2" />
+                        <div className="caption">
+                            <h3>AVATAR</h3>
+                            <p><b>Acteurs : </b>Bruce Willis, Bruce Willis & Bruce Willis</p>
+                            <p><b>Synopsis : </b>Il se passe des trucs dans ce film</p>
+                        </div>
+                    </li>
+
+                    <li className="col-md-12 row">
+                        <img src="server/img/rec.jpg" className="col-md-2" />
+                        <div className="caption">
+                            <h3>REC</h3>
+                            <p><b>Acteurs : </b>Des gens, et d'autres gens.</p>
+                            <p><b>Synopsis : </b>Il se passe aussi des trucs dans ce film</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
