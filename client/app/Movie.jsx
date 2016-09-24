71a41de75ce1375b4471208be51a5c2eda17c781
Thomas Winckell <thomas.winckell@gmail.com>
2016-09-24 19:12:48 +0200

    Affichage des boutons d'action au clic sur le film

--- client/app/Movie.jsx
+++ client/app/Movie.jsx
@@ -2,12 +2,34 @@ import React from 'react';
 
 export default class Movie extends React.Component {
 
+    state = {
+        selected : false
+    };
+
+    onSelect() {
+        this.setState({
+            selected : true
+        });
+    }
+
+    renderActionButtons(data) {
+        return (
+            <div className="pull-right">
+                <button className="btn btn-danger" onClick={() => this.props.onMovieDeletion(data.id)}>
+                    <i className="glyphicon glyphicon-trash"/>
+                </button>
+            </div>
+        );
+    }
+
     render() {
         const data = this.props.data,
               afficheUrl = data.poster || 'server/img/no-poster.jpg';
 
+        const actionButtons = this.state.selected ? this.renderActionButtons(data) : null;
+
         return (
-            <li className="col-md-12">
+            <li className="col-md-12" onClick={this.onSelect.bind(this)}>
                 <img src={afficheUrl} className="col-md-2" />
                 <div className="caption col-md-8 pull-left">
                     <h3>{data.title}</h3>
@@ -17,11 +39,7 @@ export default class Movie extends React.Component {
                     <p><b>Synopsis : </b>{data.synopsis}</p>
                     <p><b>Prix : </b>{data.price} €</p>
                 </div>
-                <div className="pull-right">
-                    <button className="btn btn-danger" onClick={() => this.props.onMovieDeletion(data.id)}>
-                        <i className="glyphicon glyphicon-trash"/>
-                    </button>
-                </div>
+                {actionButtons}
             </li>
         );
     }
