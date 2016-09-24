3de77e164aac4054488a728573623f4c80d7ade8
Thomas Winckell <thomas.winckell@gmail.com>
2016-09-24 18:17:54 +0200

    Intégration bouton de suppression

--- client/app/Movie.jsx
+++ client/app/Movie.jsx
@@ -9,13 +9,18 @@ export default class Movie extends React.Component {
         return (
             <li className="col-md-12">
                 <img src={afficheUrl} className="col-md-2" />
-                <div className="caption">
+                <div className="caption col-md-8 pull-left">
                     <h3>{data.title}</h3>
                     <p><b>Année de sortie : </b>{data.releaseYear}</p>
                     <p><b>Réalisateurs : </b>{data.directors}</p>
                     <p><b>Acteurs : </b>{data.actors}</p>
                     <p><b>Synopsis : </b>{data.synopsis}</p>
                     <p><b>Prix : </b>{data.price} €</p>
+                </div>
+                <div className="pull-right">
+                    <button className="btn btn-danger">
+                        <i className="glyphicon glyphicon-trash"/>
+                    </button>
                 </div>
             </li>
         );
